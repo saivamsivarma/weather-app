352fb33a02a6bf9e432d2f8eb6265e8d4ffe0821
Sai Vamsi Varma <saivamsivarma6@gmail.com>
2023-05-09 11:11:01 -0700

    creating react project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+export function Home(){
+    return(
+        <div className="container">
+            <div className="row justify-content-center"> {/* Placing content in center of the page */}
+                <div className="col-12 col-md-6 col-lg-4">
+                    Weather-app
+                </div>
+            </div>
+        </div>
+    )
+}
